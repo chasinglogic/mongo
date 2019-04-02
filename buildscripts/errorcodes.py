307171826763bcbdf6844dd0fecc2149ab187774
Mathew Robinson <chasinglogic@gmail.com>
2019-04-02 12:54:00 -0400

    WIP: Support Python 3 on Windows
    WIP: Fix PYTHONPATH setup for Windows

--- buildscripts/errorcodes.py
+++ buildscripts/errorcodes.py
@@ -45,21 +45,21 @@ list_files = False  # pylint: disable=invalid-name
 def parse_source_files(callback):
     """Walk MongoDB sourcefiles and invoke a callback for each AssertLocation found."""
 
-    quick = ["assert", "Exception", "ErrorCodes::Error"]
+    quick = [b"assert", b"Exception", b"ErrorCodes::Error"]
 
     patterns = [
-        re.compile(r"(?:u|m(?:sg)?)asser(?:t|ted)(?:NoTrace)?\s*\(\s*(\d+)", re.MULTILINE),
-        re.compile(r"(?:DB|Assertion)Exception\s*[({]\s*(\d+)", re.MULTILINE),
-        re.compile(r"fassert(?:Failed)?(?:WithStatus)?(?:NoTrace)?(?:StatusOK)?\s*\(\s*(\d+)",
+        re.compile(b"(?:u|m(?:sg)?)asser(?:t|ted)(?:NoTrace)?\s*\(\s*(\d+)", re.MULTILINE),
+        re.compile(b"(?:DB|Assertion)Exception\s*[({]\s*(\d+)", re.MULTILINE),
+        re.compile(b"fassert(?:Failed)?(?:WithStatus)?(?:NoTrace)?(?:StatusOK)?\s*\(\s*(\d+)",
                    re.MULTILINE),
-        re.compile(r"ErrorCodes::Error\s*[({]\s*(\d+)", re.MULTILINE)
+        re.compile(b"ErrorCodes::Error\s*[({]\s*(\d+)", re.MULTILINE)
     ]
 
     for source_file in utils.get_all_source_files(prefix='src/mongo/'):
         if list_files:
             print('scanning file: ' + source_file)
 
-        with open(source_file) as fh:
+        with open(source_file, 'rb') as fh:
             text = fh.read()
 
             if not any([zz in text for zz in quick]):
