46562af48d814df199ffafe170da223340375696
Mathew Robinson <chasinglogic@gmail.com>
2019-05-02 15:35:29 -0400

    WIP: change transitive install scanning to be configurable and pluggable

--- site_scons/site_tools/auto_install_binaries.py
+++ site_scons/site_tools/auto_install_binaries.py
@@ -62,13 +62,82 @@ def generate_alias(component, role, target="install"):
         )
 
 
-def scan_for_transitive_install(node, env, path=()):
+def get_dependent_actions(
+        components,
+        roles,
+        non_transitive_roles,
+        node,
+        cb=None,
+):
+    """
+    Check if node is a transitive dependency of components and roles
+
+    If cb is not None and is callable then it will be called with all
+    the arguments that get_dependent_actions was called with (except
+    for cb itself) as well as the results of node_roles and the
+    aib_install_actions that this function would have returned. The
+    return of cb should be the dependent actions. This allows cb to
+    access the results of scanning and modify the returned results via
+    additional filtering.
+
+    Returns the dependent actions.
+    """
+    actions = getattr(node.attributes, "aib_install_actions", None)
+    if not actions:
+        return []
+
+    # Determine if the roles have any overlap with non_transitive_roles
+    #
+    # If they are overlapping then that means we can't transition to a
+    # new role during scanning.
+    can_transfer = (
+        non_transitive_roles
+        and roles.isdisjoint(non_transitive_roles)
+    )
+
+    node_roles = {
+        role for role
+        in getattr(node.attributes, "aib_roles", set())
+        if role != "meta"
+    }
+    if (
+            # TODO: make the "always transitive" roles configurable
+            "common" not in node_roles
+            # If we are not transferrable
+            and not can_transfer
+            # Checks if we are actually crossing a boundry
+            and node_roles.isdisjoint(roles)
+    ):
+        return []
+
+    if cb is not None and hasattr(cb, '__call__'):
+        return cb(
+            components,
+            roles,
+            non_transitive_roles,
+            node,
+            node_roles,
+            actions,
+        )
+    return actions
+
+def scan_for_transitive_install(node, env, path=None, cb=None):
     """Walk the children of node finding all installed dependencies of it."""
     results = []
     install_sources = node.sources
-    print("scanning...")
-    roles = getattr(node.sources[0].attributes, "aib_roles", [])
-    is_runtime = "runtime" in roles
+    # Filter out all
+    components = {
+        component for component
+        in getattr(node.sources[0].attributes, "aib_components", set())
+        if component != "all"
+    }
+    roles = {
+        role for role
+        in getattr(node.sources[0].attributes, "aib_roles", set())
+        if role != "meta"
+    }
+    # TODO: add fancy configurability
+    non_transitive_roles = {role for role in roles if role == "runtime"}
     for install_source in install_sources:
         install_executor = install_source.get_executor()
         if not install_executor:
@@ -79,43 +148,74 @@ def scan_for_transitive_install(node, env, path=()):
         for install_target in install_targets:
             grandchildren = install_target.children()
             for grandchild in grandchildren:
-                grandroles = getattr(grandchild.attributes, "aib_roles", [])
-                print(grandchild.get_abspath(), grandroles)
-                if is_runtime and "runtime" not in grandroles:
-                    print("Crossed boundary skipping..")
-                    continue
-                actions = getattr(
-                    grandchild.attributes, "aib_install_actions", None
+                actions = get_dependent_actions(
+                    components,
+                    roles,
+                    non_transitive_roles,
+                    grandchild,
+                    cb=cb,
                 )
                 if actions:
                     results.extend(actions)
+
     # Produce deterministic output for caching purposes
     results = sorted(results, key=str)
     return results
 
 
-def collect_transitive_files(sources, env):
+def collect_transitive_files(sources, env, ignore_component_boundries=False):
     """Collect all transitive files for node."""
+    dependent_components = {}
+
+    def check_component_boundries(
+            components,
+            roles,
+            non_transitive_roles,
+            node,
+            node_roles,
+            aib_actions,
+    ):
+        # Filter out all, otherwise it would always share a component
+        node_components = {
+            component for component
+            in getattr(node.sources[0].attributes, "aib_components", set())
+            if component != "all"
+        }
+        shares_component = not node_components.isdisjoint(components)
+        if not shares_component:
+            dependent_components = dependent_components.union(node_components)
+
+        # TODO: make the always transitive roles configurable
+        #
+        # Check if we're crossing a component boundry. Unless we're one of
+        # the "special" always transitive roles we should not cross
+        # component boundries.
+        if (
+                not ignore_component_boundries
+                and "common" not in node_roles
+                and not shares_component
+        ):
+            return []
+        return aib_actions
+
     scanned_components = set()
     files = []
-    print("collecting")
-    for s in sources:
-        print(s.get_abspath())
-        component = getattr(s.attributes, "aib_components", set())
+    for source in sources:
+        component = getattr(source.sources[0].attributes, "aib_components", set())
         if not component:
-            print("no component")
             continue
 
-        diff = component.diff(scanned_components)
+        diff = component.difference(scanned_components)
         if not diff:
-            print("no diff")
             continue
 
-        print("new component")
-        components = scanned_components.union(component)
-        files += scan_for_transitive_install(s, env)
+        scanned_components = scanned_components.union(component)
+        files += scan_for_transitive_install(source, env)
 
-    return files + sources
+    return {
+        "dependent_components": dependent_components,
+        "files": files + sources,
+    }
 
 def tarball_builder(target, source, env):
     """Build a tarball of the AutoInstall'd sources."""
@@ -123,11 +223,15 @@ def tarball_builder(target, source, env):
         source = [source]
     if not source:
         return
-    files = collect_transitive_files(source, env)
+    transitive_files = collect_transitive_files(
+        source,
+        env,
+        ignore_component_boundries=True
+    )
     common_ancestor = env.Dir("$DEST_DIR").get_abspath()
-    paths = [file.get_abspath() for file in files]
+    paths = [file.get_abspath() for file in transitive_files["files"]]
     relative_files = [os.path.relpath(path, common_ancestor) for path in paths]
-    SCons.Action._subproc(
+    tar_cmd = SCons.Action._subproc(
         env,
         shlex.split(
             'tar -cvf {tarball} -C {ancestor} {files}'
@@ -138,14 +242,13 @@ def tarball_builder(target, source, env):
             )
         ),
     )
-
+    tar_cmd.wait()
 
 
 def auto_install(env, target, source, **kwargs):
     """Auto install builder."""
     target = env.Dir(env.subst(target, source=source))
     source = list(map(env.Entry, env.Flatten([source])))
-
     roles = {
         kwargs.get("ROLE_TAG"),
         # The 'meta' tag is implicitly attached as a role.
@@ -173,7 +276,7 @@ def auto_install(env, target, source, **kwargs):
     # this "hidden" argument that accepts a set.
     if kwargs.get("ADDITIONAL_COMPONENTS") is not None:
         components = components.union(set(kwargs["ADDITIONAL_COMPONENTS"]))
-        
+
     # Remove false values such as None
     roles = {role for role in roles if role}
     components = {component for component in components if component}
@@ -183,10 +286,11 @@ def auto_install(env, target, source, **kwargs):
         source=source,
     )
     for s in source:
+        s.attributes.keep_targetinfo = 1
         s.attributes.aib_install_actions = actions
         s.attributes.aib_components = components
         s.attributes.aib_roles = roles
-        
+
     for component_tag, role_tag in itertools.product(components, roles):
         alias_name = generate_alias(component_tag, role_tag)
         alias = env.Alias(alias_name, actions)
@@ -208,8 +312,8 @@ def finalize_install_dependencies(env):
         env.Alias("install", "install-default")
         env.Default("install")
 
-    installedFiles = env.FindInstalledFiles()
-    env.NoCache(installedFiles)
+    installed_files = env.FindInstalledFiles()
+    env.NoCache(installed_files)
 
     for component, rolemap in env[ALIAS_MAP].items():
         for role, info in rolemap.items():
@@ -223,10 +327,14 @@ def finalize_install_dependencies(env):
                     env.Depends(info.alias, dependency_info.alias)
 
             installed_component_files = [
-                file for file in installedFiles
+                file for file in installed_files
                 if (
                         component in getattr(file.sources[0].attributes, 'aib_components', [])
-                        and role in getattr(file.sources[0].attributes, 'aib_roles', [])
+                        and (
+                            role in getattr(file.sources[0].attributes, 'aib_roles', [])
+                            # TODO: make configurable
+                            or "common" in getattr(file.sources[0].attributes, 'aib_roles', [])
+                        )
                 )
             ]
 
@@ -237,6 +345,7 @@ def finalize_install_dependencies(env):
                 COMPONENT_TAG=component,
                 ROLE_TAG=role,
             )
+            env.NoCache(tar)
             env.Alias(tar_alias, tar)
             env.Depends(tar, info.alias)
 
@@ -286,7 +395,7 @@ def add_suffix_mapping(env, source, target=None):
                 raise Exception(
                     "target {} is not a known role. Available roles are {}"
                     .format(target, ROLES)
-                )   
+                )
 
     env[SUFFIX_MAP].update({env.subst(key): value for key, value in source.items()})
 
