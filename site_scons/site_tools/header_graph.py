c29390f99e33ac3fa107e5c3ede758bc6901e744
Mathew Robinson <chasinglogic@gmail.com>
2019-07-16 12:25:40 -0400

    WIP: generates a graph, that's neat Max said so

--- site_scons/site_tools/header_graph.py
+++ site_scons/site_tools/header_graph.py
@@ -6,20 +6,41 @@ SEEN_FILES = set()
 
 def header_graph(env, target, source):
     """Generate a header graph for source."""
+    from graphviz import Digraph
+
     if not isinstance(source, list):
         source = [source]
     source = [str(s) for s in source]
     args = env.subst(
-        'gcc $CFLAGS $CCFLAGS $CXXFLAGS -Isrc {} -H {}'.format(
+        'gcc -w -std=c11 -std=c++17 -S -Isrc {} -H {}'.format(
             " ".join(["-I" + env.Dir(d).get_abspath() for d in env['CPPPATH']]),
             " ".join(source),
         )
     )
 
-    print(args)
-    output = subprocess.check_output(shlex.split(args))
-    import pdb; pdb.set_trace()
+    output = subprocess.run(shlex.split(args), capture_output=True)
+    header_graph_lines = []
+    for line in output.stderr.decode('utf-8').split('\n'):
+        if line == "Multiple include guards may be useful for:":
+            break
+        header_graph_lines.append(line)
 
+    dot = Digraph(comment='')
+    parent_stack = []
+    level = 0
+    for header in header_graph_lines:
+        h_level = header.count(".")
+        header_name = header.replace(".", "").strip()
+        if h_level <= level:
+            for i in range(level, h_level + 1):
+                parent_stack.pop()
+        dot.node(header_name)
+        if parent_stack and parent_stack[-1]:
+            dot.edge(parent_stack[-1], header_name)
+        parent_stack.append(header_name)
+        level = h_level
+
+    dot.render('mats_graph.gv', view=True)
     return []
     
 
