8fc8ca78487b68d887de04a402b26ad48067b640
Mathew Robinson <chasinglogic@gmail.com>
2019-11-12 09:44:28 -0500

    WIP: fix cli import path

--- buildscripts/resmoke/setup.py
+++ buildscripts/resmoke/setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=requirements.splitlines(),
     entry_points={
         'console_scripts': [
-            'resmoke = resmoke.cli:main',
+            'resmoke = resmokelib.cli:main',
         ],
     },
 )
