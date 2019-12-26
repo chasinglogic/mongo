6716ae96375d41e489a454d978e0be9869a829dd
Mathew Robinson <chasinglogic@gmail.com>
2019-12-26 13:59:29 -0500

    WIP: cleanup mongo_unit and mongo_integration test tools

--- site_scons/site_tools/mongo_unittest.py
+++ site_scons/site_tools/mongo_unittest.py
@@ -12,55 +12,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-'''Pseudo-builders for building and registering unit tests.'''
+"""Pseudo-builders for building and registering unit tests."""
 from SCons.Script import Action
+
+
+def register_unit_test(env, test):
+    env.RegisterTest("$UNITTEST_LIST", test)
 
 
 def exists(env):
     return True
 
 
-_unittests = []
-def register_unit_test(env, test):
-    _unittests.append(test.path)
-    env.GenerateTestExecutionAliases(test)
-    env.Alias('$UNITTEST_ALIAS', test)
-
-def unit_test_list_builder_action(env, target, source):
-    ofile = open(str(target[0]), 'w')
-    try:
-        for s in _unittests:
-            print('\t' + str(s))
-            ofile.write('%s\n' % s)
-    finally:
-        ofile.close()
-
 def build_cpp_unit_test(env, target, source, **kwargs):
-    libdeps = kwargs.get('LIBDEPS', [])
-    libdeps.append( '$BUILD_DIR/mongo/unittest/unittest_main' )
+    libdeps = kwargs.get("LIBDEPS", [])
+    libdeps.append("$BUILD_DIR/mongo/unittest/unittest_main")
 
-    kwargs['LIBDEPS'] = libdeps
-    unit_test_components = {'tests', 'unittests'}
-    if (
-            'AIB_COMPONENT' in kwargs
-            and not kwargs['AIB_COMPONENT'].endswith('-test')
-    ):
-        kwargs['AIB_COMPONENT'] += '-test'
+    kwargs["LIBDEPS"] = libdeps
+    unit_test_components = {"tests", "unittests"}
+    if "AIB_COMPONENT" in kwargs and not kwargs["AIB_COMPONENT"].endswith("-test"):
+        kwargs["AIB_COMPONENT"] += "-test"
 
-    if 'AIB_COMPONENTS_EXTRA' in kwargs:
-        kwargs['AIB_COMPONENTS_EXTRA'] = set(kwargs['AIB_COMPONENTS_EXTRA']).union(unit_test_components)
+    if "AIB_COMPONENTS_EXTRA" in kwargs:
+        kwargs["AIB_COMPONENTS_EXTRA"] = set(kwargs["AIB_COMPONENTS_EXTRA"]).union(
+            unit_test_components
+        )
     else:
-        kwargs['AIB_COMPONENTS_EXTRA'] = unit_test_components
+        kwargs["AIB_COMPONENTS_EXTRA"] = unit_test_components
 
     result = env.Program(target, source, **kwargs)
-    env.RegisterUnitTest(result[0])
+    env.RegisterTest("$UNITTEST_LIST", result[0])
+    env.Alias("$UNITTEST_ALIAS", result[0])
 
     return result
 
 
 def generate(env):
-    env.Command('$UNITTEST_LIST', env.Value(_unittests),
-                Action(unit_test_list_builder_action, 'Generating $TARGET'))
-    env.AddMethod(register_unit_test, 'RegisterUnitTest')
-    env.AddMethod(build_cpp_unit_test, 'CppUnitTest')
-    env.Alias('$UNITTEST_ALIAS', '$UNITTEST_LIST')
+    env.TestList("$UNITTEST_LIST", source=[])
+    env.AddMethod(build_cpp_unit_test, "CppUnitTest")
+    env.AddMethod(register_unit_test, "RegisterUnitTest")
+    env.Alias("$UNITTEST_ALIAS", "$UNITTEST_LIST")
