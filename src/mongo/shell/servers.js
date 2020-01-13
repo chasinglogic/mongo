81d55fbccbb2cc565046c500848d554d0d3f461a
Mathew Robinson <chasinglogic@gmail.com>
2020-01-13 11:00:41 -0500

    wip: use ${ext} to properly upload archives, correctly detect
    programName in the shell for hygienic builds

--- src/mongo/shell/servers.js
+++ src/mongo/shell/servers.js
@@ -1034,6 +1034,10 @@ function appendSetParameterArgs(argArray) {
     // programName includes the version, e.g., mongod-3.2.
     // baseProgramName is the program name without any version information, e.g., mongod.
     let programName = argArray[0];
+    if (programName.indexOf("/") !== -1) {
+        let pathElements = programName.split("/");
+        programName = pathElements[pathElements.length - 1];
+    }
 
     let [baseProgramName, programVersion] = programName.split("-");
     let programMajorMinorVersion = 0;
